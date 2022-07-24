bc56a37ed3124919db6f766ff97c558126220f2a
vivek <vivekdixit4813@gmail.com>
2022-07-24 15:35:45 +0530

    array

--- class24.cpp
+++ class24.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main(){
      int n,search;
+     int i;
     cout <<"how many number Array is enter number" <<endl;
     cin>>n;
    
 int a[n];
 cout<<"Enetr the " <<n <<" numbers to to put in array " <<endl ;
-for(int i=0;i<n;i++){
+for(  i=0;i<n;i++){
 cout<<i <<" number \t";
 cin>>a[i];
 }
@@ -16,13 +17,14 @@ for(auto  x :a){
 }
 cout<<"now enter u want to search in it by the method of binary search"<<endl;
 cin >>search;
-for(auto x: a){
-    if (x== search){
-    cout<<x;
+for(i=0 ; i<n;i++){
+    if (search ==a[i]){
+    cout<<"the vaule u enter "<<search <<" is at the index number "<<i;
     return 0;
     }
+   
 }
-
+cout<<"the number is not found"<<endl;
 
 // cout  <<endl;
 return 0;
